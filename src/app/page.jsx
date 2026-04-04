177c217eec79b9acc9fb01af37eeebd31d3d37f8
AnkitShaw-100 <ankitshaw6933@gmail.com>
2026-04-04 12:50:00 +0530

    app layout setup done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button>Ankit</Button>
+      <Button variant="gold" size="hero">Ankit</Button>
     </div>
   );
 }
